afd0a9b4d392166cd22aab4a6778774555dd0227
Phillip Lam <phillip.lam0@gmail.com>
2021-07-20 23:53:53 -0400

    Added field validation for Number of People field

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
 // TARGETS
 const billInput = document.getElementById("bill-input");
 const peopleInput = document.getElementById("people-input");
+const peopleError = document.getElementById("error-people");
 const totalAmount = document.getElementById("total-per-person");
 const totalTip = document.getElementById("tip-per-person");
-const resetButton = document.getElementById("reset-button")
+const tipFive = document.getElementsByClassName("tip")[0];
+const tipFifteen = document.getElementsByClassName("tip")[1];
+const tipTwenty = document.getElementsByClassName("tip")[2];
+const tipTwentyFive = document.getElementsByClassName("tip")[3];
+const tipFifty = document.getElementsByClassName("tip")[4];
+const tipCustom = document.getElementsByClassName("tip")[5];
+const resetButton = document.getElementById("reset-button");
+const peopleErrorMessage = "Can't be zero.";
 
 // CALCULATE TOTALS ----------------------------------------------
-// returns the total tip based on the bill price
-function calculateTip(bill, tip) {
-    return bill * tip / 100;
-}
-
-//
-function calculateTipPerPerson(tipTotal, people) {
-    return tipTotal / people;
-}
-
-//
-function calculateTotalPerPerson(bill, tip, people) {
-    return bill * (1 + tipConverted) / people;
-}
-
 function roundTotal(total) {
     return Math.round(total * 100) / 100;
 }
@@ -37,12 +30,13 @@ function addDecimals() {                                        // This function
 
     var regex = "\\.";                                          // Regex function Search will return position of match or -1 if no match 
     var regexPosition = existingAmount.search(regex);
-    if (regexPosition == -1 && existingAmount !== "") {
+    if (regexPosition == -1 && existingAmount !== "") {         // if no decimal, add decimal
         billInput.value = existingAmount + ".00";
         totalAmount.innerHTML = existingAmount + ".00";
     }
 }
 
+// RESET BUTTON ----------------------------------------------------
 function enableReset() {
     resetButton.removeAttribute("disabled");
 }
@@ -58,6 +52,7 @@ function runReset() {
     totalAmount.innerText = "$0.00";
     totalTip.innerText = "$0.00"
     disableReset();
+    removePeopleError();
 }
 
 function detectReset() {
@@ -67,28 +62,45 @@ function detectReset() {
 }
 
 // VALIDATION --------------------------------------------------------
-function isNumPeopleZero() {
-    if (!getNumPeople()) {
-        peopleInput.setAttribute("invalid", true);
-    }
+function numPeopleZero() {
+    peopleInput.setAttribute("invalid", true);
+}
+
+function addPeopleError() {
+    peopleError.innerText = peopleErrorMessage;
+}
+
+function removePeopleError() {
+    peopleError.innerText = "";
 }
 
 // UPDATE TOTALS  ----------------------------------------------------
 
-
-
 function updateTotalPerPerson() {
     const numberPeople = getNumPeople();
     const billInputValue = billInput.value;
+    var messages = [];
 
     // Validation base case: if number of ppl is empty or 0
-    // TODO: negative case
-    if (numberPeople == "" || numberPeople <= 0) {
-        return;
+    // TODO: negative case, non-number
+    if (0) {//typeof(billInputValue) !== "number" || typeof(numberPeople) !== "number") {
+        alert("invalid");
+    }
+    if (numberPeople <= 0) {
+        console.log("test");
+        // don't calculate total
+        numPeopleZero();
+        addPeopleError();
+    // } if (numberPeople == "") {
+    //     return;
     } else {
-        // isNumPeopleZero();
+        // Check denominator first
+        removePeopleError();
+        console.log("yes");
         let totalAmountValue = billInputValue / numberPeople;
+        console.log("yes2");
         totalAmount.innerText = "$" + roundTotal(totalAmountValue);
+        console.log("yes3");
     }
 }
 
