9b62c9b7a892412d255769b0e22c1b564867e14a
Phillip <phillip.lam0@gmail.com>
2022-05-01 19:27:43 -0400

    tests: added ui test cases for bill input field

--- tests/ui-tests/tests/calculator.spec.js
+++ tests/ui-tests/tests/calculator.spec.js
@@ -6,7 +6,7 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe('Bill Input Field:', () => {
-    
+
     test('Input value has no decimals', async ({ page }) => {
 
         const td = {
@@ -26,41 +26,41 @@ test.describe('Bill Input Field:', () => {
         await expect(inpBill).toHaveValue(td.expectedBillValue);
     });
 
-    // test('Input value has decimal, but no tenth digit', async ({ page }) => {
+    test('Input value has decimal, but no tenth digit', async ({ page }) => {
 
-    //     const td = {
-    //         inputBill: '50.',
-    //         inputPeople: '2',
-    //         expectedBillValue: '50.00'
-    //     }
+        const td = {
+            inputBill: '50.',
+            inputPeople: '2',
+            expectedBillValue: '50.00'
+        }
 
-    //     let inpBill = page.locator('#bill-input');
-    //     let inpPeople = page.locator('#people-input');
+        let inpBill = page.locator('#bill-input');
+        let inpPeople = page.locator('#people-input');
 
-    //     // Input values
-    //     await inpBill.fill(td.inputBill);
-    //     await inpPeople.fill(td.inputPeople);
+        // Input values
+        await inpBill.fill(td.inputBill);
+        await inpPeople.fill(td.inputPeople);
 
-    //     // Wait for and verify input Bill input field auto-appends decimals to the hundredth place
-    //     await expect(inpBill).toHaveValue(td.expectedBillValue);
-    // });
+        // Wait for and verify input Bill input field auto-appends decimals to the hundredth place
+        await expect(inpBill).toHaveValue(td.expectedBillValue);
+    });
 
-    // test('Input value has decimal, but no hundreth digit', async ({ page }) => {
+    test('Input value has decimal, but no hundreth digit', async ({ page }) => {
 
-    //     const td = {
-    //         inputBill: '100.1',
-    //         inputPeople: '2',
-    //         expectedBillValue: '100.10'
-    //     }
+        const td = {
+            inputBill: '100.1',
+            inputPeople: '2',
+            expectedBillValue: '100.10'
+        }
 
-    //     let inpBill = page.locator('#bill-input');
-    //     let inpPeople = page.locator('#people-input');
+        let inpBill = page.locator('#bill-input');
+        let inpPeople = page.locator('#people-input');
 
-    //     // Input values
-    //     await inpBill.fill(td.inputBill);
-    //     await inpPeople.fill(td.inputPeople);
+        // Input values
+        await inpBill.fill(td.inputBill);
+        await inpPeople.fill(td.inputPeople);
 
-    //     // Wait for and verify input Bill input field auto-appends decimals to the hundredth place
-    //     await expect(inpBill).toHaveValue(td.expectedBillValue);
-    // });
+        // Wait for and verify input Bill input field auto-appends decimals to the hundredth place
+        await expect(inpBill).toHaveValue(td.expectedBillValue);
+    });
 });
